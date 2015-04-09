e2ab9a177fb3ba0d1165cb4f2aad6b6cc9a1eb10
sunnankar <sunnankar@users.noreply.github.com>
2015-04-09 01:04:22 -0400

    Update btcquote-widget.js
    Fix linkback URL.

--- bitcoin-price/src/btcquote-widget.js
+++ bitcoin-price/src/btcquote-widget.js
@@ -190,7 +190,7 @@ var BTCQuote = function () {
 							'<b style="font-weight: bold; font-size: 10px; color: #999;">$</b><b id="btc-ask-field" style="font-weight: bold; font-size: 10px; color: #999;"></b>',
 						'</span>',
 						'<span style="position: absolute; right: 0px; top: 14px; color: #666; text-align: right; width: 160px; font-size: 10px;">',
-							'Powered by <a href="http://www.weusecoins.com/" target="_blank" style="background-color: inherit; float: right; margin-left: 4px; color: #666; text-decoration: underline; cursor: pointer; font-size: 10px; padding: 0px;">WeUseCoins.com</a>',
+							'Powered by <a href="http://www.weusecoins.com/en/bitcoin-price" target="_blank" style="background-color: inherit; float: right; margin-left: 4px; color: #666; text-decoration: underline; cursor: pointer; font-size: 10px; padding: 0px;">WeUseCoins.com</a>',
 						'</span>',
 					'</div>',
 				'</div>',
